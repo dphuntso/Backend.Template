b71b0fa2dd5d3147415c684a88b6d45301af181c
unknown <dorjeetsewang239@gmail.com>
2022-11-07 11:46:44 -0500

    dotenv, get PORT from process.env

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const port = require("./config.js").port;
 const app = express();
 
 const controller = require("./src/controllers/controller.js");
 controller(app);
 
+// const port = require("./config.js").port;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log("app is listening to " + port);
 })
